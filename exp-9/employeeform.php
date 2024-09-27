5793c3da74c29d50476fdba784cb7f1b07912913
Sakthi M S <220701240@rajalakshmi.edu.in>
2024-09-27 23:29:04 +0530

    Create employeeform.php

---
+++ exp-9/employeeform.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Employee Details</title>
+</head>
+<body>
+    <h2>Get Employee Details</h2>
+    <form action="get_employee.php" method="post">
+     
+  Employee id:  <input type="text" id="empid" name="empid" required><br>
+    Name    :<input type="text" id="Name" name="name" required><br>
+Desig :<input type="text" id="desig" name="desig" required><br>
+
+ salary:   <input type="text" id="salary" name="salary" required>
+ 
+
+        <input type="submit" value="Get Details">
+    </form>
+</body>
+</html>
