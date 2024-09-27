9cdb5c0a00568615e93025be156198157b69c852
Sakthi M S <220701240@rajalakshmi.edu.in>
2024-09-27 23:23:22 +0530

    Create accountform.php

---
+++ exp-8/accountform.php
@@ -0,0 +1,9 @@
+<form method="POST" action="account.php">
+    <label>Account Type (S or C):</label>
+    <input type="text" name="atype">
+    <label>Balance:</label>
+    <input type="text" name="balance">
+    <label>Customer ID:</label>
+    <input type="text" name="cid">
+    <input type="submit" value="Add Account">
+</form>
