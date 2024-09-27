1f36dfd332d403149118b823f0f730e677a122d4
Sakthi M S <220701240@rajalakshmi.edu.in>
2024-09-27 23:29:47 +0530

    Create updateemployeeform.php

---
+++ exp-9/updateemployeeform.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Employee Details</title>
+</head>
+<body>
+    <h2>Get Employee Details</h2>
+    <form action="update_employee.php" method="post">
+     
+  Employee id:  <input type="text" id="empid" name="empid" required><br>
+   
+Desig :<input type="text" id="desig" name="desig" required><br>
+
+ 
+
+        <input type="submit" value="Update Details">
+    </form>
+</body>
+</html>
