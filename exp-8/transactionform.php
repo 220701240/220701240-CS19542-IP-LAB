8d280d081937c771807e864b2fd89544faee6fa9
Sakthi M S <220701240@rajalakshmi.edu.in>
2024-09-27 23:24:55 +0530

    Create transactionform.php

---
+++ exp-8/transactionform.php
@@ -0,0 +1,10 @@
+
+<form method="POST" action=transactions.php>
+    <label>Account No:</label>
+    <input type="text" name="ano">
+    <label>Transaction Type (D or W):</label>
+    <input type="text" name="ttype">
+    <label>Amount:</label>
+    <input type="text" name="tamount">
+    <input type="submit" value="Submit Transaction">
+</form>
